a0fc4eeb624c107cd14cc4ec87dec7d1ff4c9080
JuanManuelRivera31 <juanm.rivera3105@gmail.com>
2025-02-14 01:27:43 -0500

    Carrito persistente con local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+    //El state de react es asincrono //STATE ES INMUTABLE
 import Guitar from "./components/Guitar"
 import Header from "./components/Header"
 import { db } from "./data/db"
 
 function App() {
-  //STATE ES INMUTABLE
-  const [data, setData]= useState(db); //Se inicializa de una vez ya que es un archivo local
+
+  const initialCart = () => {
+    const localStorageCart= localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+    //Si local storage tiene algo va setearlo a arreglo si no su valor inicial es un arreglo vacio
+  }
+
+  const [data]= useState(db) //Se inicializa de una vez ya que es un archivo local
   //Si consumieramos una api usariamos use effect ya que no sabemos cuanto demora en cargar datos
-  const [cart, setCart]= useState([]);//Inicializamos el carrito vacio y lo vamos a ir llenando o modificando con ciertos métodos array
+  const [cart, setCart]= useState(initialCart)//Inicializamos el carrito vacio y lo vamos a ir llenando o modificando con ciertos métodos array
+
   const MAX_ITEMS= 5
   const MIN_ITEMS= 1
+
+  useEffect(() => { //Va sincronizar - Use Effect para los efectos secundarios cuando nuestro state cambia
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart]) //Cada que cart cambie realiza el codigo anterior
 
   function addToCart(item){
     const itemExists = cart.findIndex(guitar => guitar.id === item.id)
     if(itemExists >= 0){ //Existe en el carrito
-      if(cart[itemExists].quantity >= MAX_ITEMS) return 
+      if(cart[itemExists].quantity >= MAX_ITEMS) return
       const updatedCart = [...cart] //Copia del carrito con spread operator(...)
       updatedCart[itemExists].quantity++
       setCart(updatedCart)//Sin modificar state original porque estamos tomando copia
-      
+
     } else{ //Seteamos el carrito de compras y lo colocamos en nuestro state
       item.quantity= 1
       setCart([...cart, item]) //La funcion setCart sabe lo que hay en el state, toma una copia del state y escribe el nuevo elemento State
@@ -61,7 +73,7 @@ function App() {
 
   return (
     <>
-    <Header 
+    <Header
       cart={cart}
       removeFromCart={removeFromCart}
       increaseQuantity={increaseQuantity}
@@ -73,12 +85,12 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-            {data.map((guitar) => ( //Accedemos a data (state), utilizamos un array method (map) y el arreglo que nos retornaa lo nombramos guitar 
+            {data.map((guitar) => ( //Accedemos a data (state), utilizamos un array method (map) y el arreglo que nos retornaa lo nombramos guitar
               <Guitar //Iteramos y generamos un componente Guitar por cada elemento en ese arreglo
               key={guitar.id} // Prop especial que siempre debemos utilizar cuando iteremos una lista y le pasamos un valor único
               guitar={guitar}
               setCart={setCart}//Prop=Funcion
-              addToCart={addToCart}//Cada componente va tener la funcionalidad de agg ese elemento al carrito 
+              addToCart={addToCart}//Cada componente va tener la funcionalidad de agg ese elemento al carrito
               />
             ))}
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,104 +3,102 @@ import { useMemo } from "react"
 export default function Header({cart, removeFromCart, increaseQuantity, decrementQuantity, clearCart}){//Tomamos nuestro carrito de compras
 
     //State Derivado
-    const isEmpty= useMemo(() => cart.length === 0, [cart]) //Derivado porque depende del state carrito
-    const cartTotal= useMemo(() => cart.reduce( (total, item) => total + (item.quantity * item.price), 0)
-    ,[cart]) 
+    const isEmpty= useMemo( () => cart.length === 0, [cart]) //Derivado porque depende del state carrito
+    const cartTotal= useMemo( () => cart.reduce( (total, item) => total + (item.quantity * item.price), 0), [cart]) 
     //Total(Acumulado luego de iterar sobre los elementos del carrito)
     
     //La idea de poner el codigo acá arriba es sacar la lógica de nuestro return
 
     return (
         <header className="py-5 header">
-        <div className="container-xl">
-            <div className="row justify-content-center justify-content-md-between">
-                <div className="col-8 col-md-3">
-                    <a href="index.html">
-                        <img className="img-fluid" src="./public/img/logo.svg" alt="imagen logo" />
-                    </a>
-                </div>
-                <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
-                    <div 
-                        className="carrito"
-                    >
-                        <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
-
-                        <div id="carrito" className="bg-white p-3">
-                            {isEmpty ? ( //True si el carrito está vacio - False si no tiene nd 
-                                <p className="text-center">El carrito esta vacio</p>
-                            ) : (
-                        <>
-                            <table className="w-100 table">
-                                <thead>
-                                    <tr>
-                                        <th>Imagen</th>
-                                        <th>Nombre</th>
-                                        <th>Precio</th>
-                                        <th>Cantidad</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {cart.map( guitar => (//Iterar sobre el carrito y nos crea el objeto guitar
-                                    //Itera y ejecuta este codigo una vez por cada elemento en mi carrito
-                                     <tr key={guitar.id}> 
-                                     {/* //// Prop especial que siempre debemos utilizar cuando iteremos una lista y le pasamos un valor único - ID unico para evitar registros duplicados */}
-                                        <td>
-                                            <img 
-                                                className="img-fluid" 
-                                                 src={`/img/${guitar.image}.jpg`} //Template string (``) Para inyectar esa variable img
-                                                alt="imagen guitarra" />
-                                        </td>
-                                        <td>{guitar.name}</td>
-                                        <td className="fw-bold">
-                                            {guitar.price}
-                                        </td>
-                                        <td className="flex align-items-start gap-4">
-                                            <button
-                                                type="button"
-                                                className="btn btn-dark"
-                                                onClick={() => decrementQuantity(guitar.id)}
-                                            >
-                                                -
-                                            </button>
-                                                {guitar.quantity}
-                                            <button
-                                                type="button"
-                                                className="btn btn-dark"
-                                                onClick={() => increaseQuantity(guitar.id)}
-                                            >
-                                                +
-                                            </button>
-                                        </td>
-                                        <td>
-                                            <button
-                                                className="btn btn-danger"
-                                                type="button"
-                                                onClick={() => removeFromCart(guitar.id)} //Como toma un parametro debe usar call back () =>
-                                            >
-                                                X
-                                            </button>
-                                        </td>
-                                    </tr>
-                                    ))} 
-                                    {/* //Retorna lo anterior */}
-                                </tbody>
-                            </table>
-                            
-                            <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal}</span></p>
-                            <button 
-                            className="btn btn-dark w-100 mt-3 p-2"
-                            onClick={clearCart}
-                            >
-                                Vaciar Carrito
-                                </button>
-                            </>
-                        )}
-                            </div>
+            <div className="container-xl">
+                <div className="row justify-content-center justify-content-md-between">
+                    <div className="col-8 col-md-3">
+                        <a href="index.html">
+                            <img className="img-fluid" src="/img/logo.svg" alt="imagen logo" />
+                        </a>
                     </div>
-                </nav>
+                    <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
+                        <div 
+                            className="carrito"
+                        >
+                            <img className="img-fluid" src="/img/carrito.png" alt="imagen carrito" />
+
+                            <div id="carrito" className="bg-white p-3">
+                                {isEmpty ? ( //True si el carrito está vacio - False si no tiene nd 
+                                    <p className="text-center">El carrito esta vacio</p>
+                                ) : (
+                            <>
+                                <table className="w-100 table">
+                                    <thead>
+                                        <tr>
+                                            <th>Imagen</th>
+                                            <th>Nombre</th>
+                                            <th>Precio</th>
+                                            <th>Cantidad</th>
+                                            <th></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {cart.map( guitar => (//Iterar sobre el carrito y nos crea el objeto guitar
+                                        //Itera y ejecuta este codigo una vez por cada elemento en mi carrito
+                                        <tr key={guitar.id}> 
+                                        {/* //// Prop especial que siempre debemos utilizar cuando iteremos una lista y le pasamos un valor único - ID unico para evitar registros duplicados */}
+                                            <td>
+                                                <img 
+                                                    className="img-fluid" 
+                                                    src={`/img/${guitar.image}.jpg`} //Template string (``) Para inyectar esa variable img
+                                                    alt="imagen guitarra" />
+                                            </td>
+                                            <td>{guitar.name}</td>
+                                            <td className="fw-bold">
+                                                {guitar.price}
+                                            </td>
+                                            <td className="flex align-items-start gap-4">
+                                                <button
+                                                    type="button"
+                                                    className="btn btn-dark"
+                                                    onClick={() => decrementQuantity(guitar.id)}
+                                                >
+                                                    -
+                                                </button>
+                                                    {guitar.quantity}
+                                                <button
+                                                    type="button"
+                                                    className="btn btn-dark"
+                                                    onClick={() => increaseQuantity(guitar.id)}
+                                                >
+                                                    +
+                                                </button>
+                                            </td>
+                                            <td>
+                                                <button
+                                                    className="btn btn-danger"
+                                                    type="button"
+                                                    onClick={() => removeFromCart(guitar.id)} //Como toma un parametro debe usar call back () =>
+                                                >
+                                                    X
+                                                </button>
+                                            </td>
+                                        </tr>
+                                        ))} 
+                                        {/* //Retorna lo anterior */}
+                                    </tbody>
+                                </table>
+                                
+                                <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal}</span></p>                             
+                                </>
+                            )}
+                                <button 
+                                    className="btn btn-dark w-100 mt-3 p-2"
+                                    onClick={clearCart}
+                                >Vaciar Carrito</button>
+                            
+                            </div>
+                        </div>
+                    </nav>
+                </div>
             </div>
-        </div>
-    </header>
+        </header>
     )
 }
